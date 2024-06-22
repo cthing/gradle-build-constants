ba8267335f54443ee5ef9084a1d0b7dce1bf13c6
Baron Roberts <baron@cthing.com>
2024-06-21 23:42:10 -0700

    Prepare for next release.

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ plugins {
     alias(libs.plugins.versions)
 }
 
-version = ProjectVersion("1.0.0", BuildType.release)
+version = ProjectVersion("1.0.1", BuildType.snapshot)
 group = "org.cthing"
 description = "A Gradle plugin that generates a Java source file with constants describing the build."
 
